bfcb042edf6c4d94035f75c04c0ad933d85e08ce
Valentin Buck <apexys@toppoint.de>
2016-12-09 14:08:00 +0100

    Added error reporting

--- Node-ES6/promisetest.js
+++ Node-ES6/promisetest.js
@@ -18,6 +18,11 @@ let items = 0;
 var request = require('request');
 var makeReq = (url) => new Promise((resolve, reject)=>{
     request(url, function (error, response, body) {
+	 if(error){
+		console.log("Error at request " + url + ": " + error);
+		resolve("ERROR");
+		return;
+	 }
          console.log("Got " + url + "(" + response.statusCode + ")") ;
          items ++;
 	 console.log(items);
